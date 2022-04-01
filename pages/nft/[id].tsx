ea4c0af8f55a1458064655d5dbfe3c7289a6e087
Jack Buscombe <jackbuscombe94@gmail.com>
2022-04-01 21:28:59 +1000

    First Build

---
+++ pages/nft/[id].tsx
@@ -0,0 +1,18 @@
+function NFTDropPage() {
+  return (
+    <div className="flex h-screen flex-col">
+      {/* Left */}
+      <div className="bg-gradient-to-br from-cyan-800 to-rose-500">
+        <img
+          className="w-44 rounded-xl object-cover"
+          src="https://links.papareact.com/8sg"
+          alt=""
+        />
+      </div>
+
+      {/* Right */}
+      <div></div>
+    </div>
+  )
+}
+export default NFTDropPage
